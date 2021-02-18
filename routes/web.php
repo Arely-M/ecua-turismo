8feffcbf4ad2e31a03e1db11a4ed1db835ef71dd
Gustavo <gus-tavo99@outlook.es>
2021-02-17 21:39:12 -0500

    Modelos, Controladores, Rutas

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\rolController;
+use App\Http\Controllers\permisoController;
+use App\Http\Controllers\personaController;
+use App\Http\Controllers\publicacionController;
+use App\Http\Controllers\usuarioController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('rol', rolController::class);
+Route::resource('permiso', permisoController::class);
+Route::resource('persona', personaController::class);
+Route::resource('publicacion', publicacionController::class);
+Route::resource('usuario', usuarioController::class);
